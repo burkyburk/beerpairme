065fbd2b544f733277a3421dc6d52d91f4e8fa32
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-03-10 17:40:19 -0500

    working with api to search for beers

--- assets/js/app/controller.js
+++ assets/js/app/controller.js
@@ -1,8 +1,28 @@
 angular.module('app.controllers', [])
-.controller('homeCtrl', function() {
+.controller('homeCtrl', function($scope, $http) {
 
-})
+    	var gameCollection = [];
+    	var sort = true;
+        $scope.beers = [];
+        $scope.searchParam = '';
 
+    	$scope.searchSubmit=function(searchParam){
+    		if(searchParam.length < 2) {
+    			$scope.errorMsg = "Search requires 2 or more characters";
+    		}
+    		$http.get('http://api.brewerydb.com/v2/search?key=5e6ff5f6aa57d35101352aff2579b986&q='+searchParam+'&type=beer&withBreweries=y')
+    			.success(function(response) {
+                    $scope.beers = response.data;
+                    console.log(response);
+    				$scope.showTable = true;
+                    console.log($scope.beers);
+    			})
+    			.error(function(err) {
+    				console.log(err);
+    			});
+    		};
+
+    })
 .controller('newpairingsCtrl', function() {
 
 })
