a3d83d731aea1d4210a50f926cbf03a1c1caf4d8
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-03-21 15:12:36 -0500

    added more beer styles, and submit form is working

--- .tmp/public/js/app/controller.js
+++ .tmp/public/js/app/controller.js
@@ -16,12 +16,11 @@ angular.module('app.controllers', ['ngTable'])
     		$http.get('/beer/search?query='+searchParam)
     			.success(function(response) {
                     $scope.beers = response.data;
-                    console.log(response);
     				$scope.showTable = true;
 
                     $scope.tableParams = new ngTableParams({
                         page: 1,            // show first page
-                        count: 15,          // count per page
+                        count: 50,          // count per page
                         sorting: {
                             name: 'asc'     // initial sorting
                         },
@@ -33,7 +32,6 @@ angular.module('app.controllers', ['ngTable'])
                             var orderedData = params.sorting() ?
                                                 $filter('orderBy')($scope.beers, params.orderBy()) :
                                                 $scope.beers;
-                                                    console.log(params.orderBy());
                             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
                         }
                     });
@@ -47,7 +45,7 @@ angular.module('app.controllers', ['ngTable'])
 
 
 })
-.controller('newpairingsCtrl', function($scope, $http) {
+.controller('newpairingsCtrl', function($scope, $http, ngTableParams, $filter) {
 
             $scope.newpairings = [];
 
@@ -59,6 +57,26 @@ angular.module('app.controllers', ['ngTable'])
                     }
                     $scope.newpairings = response;
                     console.log(response);
+                    $scope.tableParams = new ngTableParams({
+                        page: 1,            // show first page
+                        count: 40,          // count per page
+                        sorting: {
+                            createdAt: 'desc'
+                                // initial sorting
+                        },
+                        counts:''
+                    }, {
+                        total: $scope.newpairings.length, // length of data
+                        getData: function($defer, params) {
+                            // use build-in angular filter
+                            var orderedData = params.sorting() ?
+                                                $filter('orderBy')($scope.newpairings, params.orderBy()) :
+                                                $scope.newpairings;
+                            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                        }
+                    });
+
+
                 })
                 .error(function(err)    {
                     console.log(err);
@@ -78,8 +96,6 @@ angular.module('app.controllers', ['ngTable'])
                 $http.get('/Recipe?style='+style)
                 .success(function(response) {
                     $scope.beerstyle = response[0];
-                    console.log(response);
-                    console.log($scope.beerstyle.pairings);
 
                 })
                 .error(function(err)   {
@@ -91,18 +107,37 @@ angular.module('app.controllers', ['ngTable'])
                     GetRecipe('Hefeweizen');
                 else if ($scope.beerstyle.indexOf('India Pale Ale') !==-1)
                         GetRecipe('India Pale Ale');
+                else if ($scope.beerstyle.indexOf('Imperial Stout') !==-1)
+                        GetRecipe('Imperial Stout');
                 else if ($scope.beerstyle.indexOf('Stout') !==-1)
                         GetRecipe('Stout');
                 else if ($scope.beerstyle.indexOf('Porter') !==-1)
                         GetRecipe('Porter');
                 else if ($scope.beerstyle.indexOf('Pilsener') !==-1)
                         GetRecipe('Pilsner');
+                else if ($scope.beerstyle.indexOf('Amber') !==-1)
+                        GetRecipe('Amber');
+                else if ($scope.beerstyle.indexOf('Smoke') !==-1)
+                        GetRecipe('Smoke');
                 else if ($scope.beerstyle.indexOf('Lager') !==-1)
                         GetRecipe('Lager');
                 else if ($scope.beerstyle.indexOf('Bock') !==-1)
                         GetRecipe('Bock');
                 else if ($scope.beerstyle.indexOf('Lambic') !==-1)
                         GetRecipe('Lambic');
+                else if ($scope.beerstyle.indexOf('Pale Ale') !==-1)
+                        GetRecipe('Pale Ale');
+                else if ($scope.beerstyle.indexOf('Sour') !==-1)
+                        GetRecipe('Sour');
+                else if ($scope.beerstyle.indexOf('Saison') !==-1)
+                        GetRecipe('Saison');
+                else if ($scope.beerstyle.indexOf('Strong') !==-1)
+                        GetRecipe('Strong Ale');
+                else if ($scope.beerstyle.indexOf('Barley Wine') !==-1)
+                        GetRecipe('Barley Wine');    
+
+
+
             }
 
 
@@ -113,7 +148,6 @@ angular.module('app.controllers', ['ngTable'])
                     $scope.beerdetails = response.data;
                     $scope.beerstyle = $scope.beerdetails.style.name;
                     styleParse($scope.beerstyle);
-                    console.log($scope.beerdetails);
 
 
           })
@@ -122,15 +156,94 @@ angular.module('app.controllers', ['ngTable'])
           });
 
 
-      console.log($stateParams.beerId);
-
-
-
 })
 .controller('infoCtrl', function() {
 
 })
 
-.controller('submitCtrl', function() {
+.controller('submitCtrl', function($scope, $http) {
+    $scope.submitSuccess = true;
+    $scope.myStyle = '';
+    $scope.myPairings = '';
+    $scope.myUrl ='';
+
+    $scope.$watch('myStyle', function() {
+		if(angular.isUndefined($scope.myStyle) || $scope.myStyle === '') {
+			$scope.class1 = 'errorClass';
+		} else {
+			$scope.class1 = 'none';
+		}
+	});
+	$scope.$watch('myPairings', function() {
+		if(angular.isUndefined($scope.myPairings) || $scope.myPairings === '') {
+			  $scope.class2 = 'errorClass';
+		}	else {
+              $scope.class2 = 'none';
+
+		}
+	});
+	$scope.$watch('myUrl', function() {
+		if(angular.isUndefined($scope.myUrl) || $scope.myUrl === '') {
+			$scope.class3 = 'errorClass';
+		} else {
+			if($scope.myUrl.substring(0,7) === 'http://') {
+				$scope.class3 = 'none';
+			} else {
+				$scope.class3 = 'errorClass';
+			}
+		}
+	});
+
+    $scope.submit = function() {
+		var submitPass = 0;
+
+		var errorArray = [
+			'Style cannot be left empty',
+			'Pairing Info cannot be left empty',
+			'Link cannot be left empty',
+			'Link must start with http://',
+		];
+
+		if(angular.isUndefined($scope.myStyle) || $scope.myStyle === '') {
+			$scope.error1 = errorArray[0];
+		} else {
+			$scope.error1 = undefined;
+			submitPass++;
+		}
+		if(angular.isUndefined($scope.myPairings) || $scope.myPairings === '') {
+			$scope.error2 = errorArray[1];
+		} else {
+    		$scope.error2 = '';
+			submitPass++;
+        }
+		if(angular.isUndefined($scope.myUrl) || $scope.myUrl === '') {
+			$scope.error3 = errorArray[3];
+		} else {
+			if($scope.myUrl.substring(0,7) === 'http://') {
+				$scope.error3 = '';
+				submitPass++;
+			} else {
+				$scope.error3 = errorArray[4];
+			}
+		}
+
+
+		if(submitPass === 3) {
+			$scope.submitSuccess = false;
+		}
+	};
+
+    $scope.submitClick = function(myStyle, myPairings, myUrl) {
+    $http.post(
+        '/Recipe',
+        {
+            style: myStyle,
+            pairings: myPairings,
+            url: myUrl
+        }
+    );
+
+
+    };
 
 });
